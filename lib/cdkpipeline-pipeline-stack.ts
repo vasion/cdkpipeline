96893c076b917cd158cc4df7a062378cdf6d9523
Momchil Rogelov <momchilrogelov@gmail.com>
2020-08-23 19:30:51 +0100

    Prod and validation

--- lib/cdkpipeline-pipeline-stack.ts
+++ lib/cdkpipeline-pipeline-stack.ts
@@ -1,7 +1,7 @@
 import * as codepipeline from "@aws-cdk/aws-codepipeline";
 import * as codepipeline_actions from "@aws-cdk/aws-codepipeline-actions";
 import { Construct, SecretValue, Stack, StackProps } from "@aws-cdk/core";
-import { CdkPipeline, SimpleSynthAction } from "@aws-cdk/pipelines";
+import { CdkPipeline, SimpleSynthAction, CdkStage, ShellScriptAction } from "@aws-cdk/pipelines";
 import { CdkpipelineStage } from "./cdkpipeline-stage";
 
 export class CdkpipelinePipelineStack extends Stack {
@@ -29,7 +29,21 @@ export class CdkpipelinePipelineStack extends Stack {
         });
 
         // Stages
-        pipeline.addApplicationStage(new CdkpipelineStage(this, "PreProd", {
+        const preprod = new CdkpipelineStage(this, "PreProd", {
+            env: { account: "471276646031", region: "eu-west-1" }
+        });
+        const preProdStage: CdkStage = pipeline.addApplicationStage(preprod);
+        preProdStage.addActions(new ShellScriptAction({
+            actionName: "TestService",
+            useOutputs: {
+                ENDPOINT_URL: pipeline.stackOutput(preprod.urlOutput)
+            },
+            commands: [
+                'curl -Ssf $ENDPOINT_URL'
+            ]
+        }));
+
+        pipeline.addApplicationStage(new CdkpipelineStage(this, "Prod", {
             env: { account: "471276646031", region: "eu-west-1" }
         }));
     }
